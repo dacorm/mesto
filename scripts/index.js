0788ee7c118f60f3f927f638870b3c35c221e02d
Utkin Denis <stolen.youth@yandex.ru>
2022-05-28 02:42:58 +0700

    feat: cards from array

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,74 @@
 const popupOpened = document.querySelector('.profile__edit-button');
-const popupClosed = document.querySelector('.popup__escape-button');
-const popup = document.querySelector('.popup');
-const formElement = document.querySelector('.popup__form');
+const popupClose = document.querySelector('#edit_close');
+const popupProfile = document.querySelector('#popup_profile');
+const formElement = document.querySelector('#profile_form');
 const nameInput = document.querySelector('#name');
 const jobInput = document.querySelector('#workplace');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__workplace');
-const likes = document.querySelectorAll('.card__item-like-button')
+const placeInput = document.querySelector('#new_place');
+const placeImageInput = document.querySelector('#place_link');
+const placeForm = document.querySelector('#place_form');
+const placeAddButton = document.querySelector('.profile__add-button');
+const popupPlace = document.querySelector('#popup_place');
+const placeClose = document.querySelector('#place_close');
+const cardItems = document.querySelector('.card__items');
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 
-for (const like of likes) {
-    like.addEventListener('click', () => {
-        like.classList.toggle('card__item-like-button_active');
-    });
+for (let i = 0; i < initialCards.length; i++) {
+    fillPlace(i);
 }
 
-function open() {
-    popup.classList.add('popup_active');
+function fillPlace(i) {
+    const cardTemplate = document.querySelector('#card').content;
+    const card = cardTemplate.querySelector('.card__item').cloneNode(true);
+
+    card.querySelector('.card__item-image').src = `${initialCards[i].link}`;
+    card.querySelector('.card__item-title').textContent = `${initialCards[i].name}`;
+    card.querySelector('.card__item-like-button').addEventListener('click', (event) => {
+        const evtTarget = event.target;
+        evtTarget.classList.toggle('card__item-like-button_active');
+    })
+    cardItems.append(card);
+}
+
+
+
+
+function open(element) {
+    element.classList.add('popup_active');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
-function close() {
-    popup.classList.remove('popup_active');
+function close(element) {
+    element.classList.remove('popup_active');
 }
 
 
@@ -30,9 +76,24 @@ function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    close();
+    close(popupProfile);
 }
 
-popupOpened.addEventListener('click', open);
-popupClosed.addEventListener('click', close);
+function formSubmitHandlerPlace(evt) {
+    evt.preventDefault();
+    close(popupProfile);
+}
+
+popupOpened.addEventListener('click', () => {
+    open(popupProfile);
+});
+popupClose.addEventListener('click', () => {
+    close(popupProfile)
+});
+placeClose.addEventListener('click', () => {
+    close(popupPlace)
+});
+placeAddButton.addEventListener('click', () => {
+    open(popupPlace);
+});
 formElement.addEventListener('submit', formSubmitHandler);
